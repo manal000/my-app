062a1aacd86afbb4b644d03c8acf7cc13802298a
bennan <lilac161@gmail.com>
2022-07-03 14:37:17 +0100

    update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <header className="App-header">
 <Header />
    
-<Login />
+<Client />
       </header>
     
     </div>
--- src/containers/Client/Client.js
+++ src/containers/Client/Client.js
@@ -10,9 +10,89 @@ function Client() {
 
     <div className="test" test>
      <p className="View"> AFFICHER <b className="client">CLIENT </b></p>
-     <input type="text" id="search" placeholder="Search.."></input>
-     </div>
+     <input type="text" id="search" placeholder="Search.."></input> 
+     
          </div>
+<div className="grid-client">
+
+<div className="cardClient">
+  <span id="name">
+name
+  </span> 
+  <span id="id">
+    id
+  </span> <br />
+  <span id="address">
+    address
+  </span> <br />
+  <span id="tel">
+    tel
+  </span>
+</div>
+
+<div className="cardClient">
+  <span id="name">
+name
+  </span> 
+  <span id="id">
+    id
+  </span> <br />
+  <span id="address">
+    address
+  </span> <br />
+  <span id="tel">
+    tel
+  </span>
+</div>
+<div className="cardClient">
+  <span id="name">
+name
+  </span> 
+  <span id="id">
+    id
+  </span> <br />
+  <span id="address">
+    address
+  </span> <br />
+  <span id="tel">
+    tel
+  </span>
+</div>
+<div className="cardClient">
+  <span id="name">
+name
+  </span> 
+  <span id="id">
+    id
+  </span> <br />
+  <span id="address">
+    address
+  </span> <br />
+  <span id="tel">
+    tel
+  </span>
+</div>
+<div className="cardClient">
+  <span id="name">
+name
+  </span> 
+  <span id="id">
+    id
+  </span> <br />
+  <span id="address">
+    address
+  </span> <br />
+  <span id="tel">
+    tel
+  </span>
+</div>
+
+</div>
+
+
+</div>
+
+         
 
 
 
