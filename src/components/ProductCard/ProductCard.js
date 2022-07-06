24549ab20bc0f7ab73dd61e624ca376df7b17bb1
bennan <lilac161@gmail.com>
2022-07-06 10:46:16 +0100

    new

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import './ProductCard.css';
+import "./ProductCard.css";
 
 const ProductCard = () => {
   return (
     <div>
-      <div className='grid-product'>
-        <div>  <img src="/product1.webp" class="product1" ></img> </div>
-        <div className='nameProduct'>coca cola zero</div>
-        <input id="qte" type="number" min="0"  max="100"></input>
-        <div className='descriptionProduct'>25cl</div>
-        <div className='price'>4.49 DH</div>
-        <input type="submit" onclick="myFunction()" id="button-add-cart" value ="Ajouter au panier" ></input>
+      <div class="grid-product">
+        <div id="product1">  <img src="/product1.webp" class="product1" ></img> </div>
+        <div id="nameProduct">COCA COLA ZERO</div>
+        <div id="qte"><input id="qte" type="number" min="0"  max="100" ></input></div>
+        <div id="descriptionProduct">25cl</div>
+        <div id="price">4.49 DH</div>
+        <div id="button-add-cart"><input type="submit" onclick="myFunction()" id="button-add-cart" value ="Ajouter au panier" ></input> </div>
     
       </div>
     </div>
